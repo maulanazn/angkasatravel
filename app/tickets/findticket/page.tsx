e19d11455b36b5d2952b7dfcadaf8eff4cc989e8
maulanazn <Starknntnk99@gmail.com>
2023-09-12 22:00:41 +0700

    adding modal after search in find tickets page

--- app/tickets/findticket/page.tsx
+++ app/tickets/findticket/page.tsx
@@ -1,7 +1,43 @@
+import BirdLogo from '@/public/tickets/plane-fly-logo-big.png'
+import BirdLogoSmall from '@/public/tickets/plane-fly-logo-small.png'
+import {IoSwapHorizontalSharp} from 'react-icons/io5';
+import {IoEllipse} from 'react-icons/io5';
+import Image from 'next/image'
+import React from 'react'
+import Link from 'next/link';
+
 export default function FindTickets() {
     return (
         <main className="m-0 p-0">
-            <h1 className="text-5xl text-blue-700 text-center lg:mt-10">Find Tickets</h1>
+            <section className="lg:bg-blue-500 lg:w-[85.4rem] lg:h-32 lg:rounded-b-3xl lg:ml-18">
+                <Image src={BirdLogo} alt="bird logo" width="150" height="150"/>
+                <Image src={BirdLogoSmall} alt="bird logo" className="lg:mt-[-5rem] lg:ml-20" width="40" height="40"/>
+                <div className="lg:gap-10 lg:mt-[-2rem] lg:ml-32 text-white">
+                    <table>
+                        <tbody>
+                            <td>from</td>
+                            <td> </td>
+                            <td  className='px-8'>to</td>
+                        </tbody>
+                        <tbody>
+                            <td>Medan</td>
+                            <td><IoSwapHorizontalSharp size={30}/></td>
+                            <td  className='px-8'>Tokyo</td>
+                        </tbody>
+                        <tbody>
+                            <td>Monday, 20 July 2020</td>
+                            <td  className='px-2'><IoEllipse/></td>
+                            <td>4 Passengers</td>
+                            <td><IoEllipse/></td>
+                            <td  className='px-2'>Economy</td>
+                        </tbody>
+                    </table>
+                </div>
+                <Link href="#" className='flex text-white flex-row justify-end items-end lg:mt-[-4rem] lg:mr-[2rem]'>
+                    Change Search
+                </Link>
+            </section>
+            <h1 className="text-5xl text-blue-700 text-left lg:mt-10">Select a Ticket</h1>
         </main>
     )
 }
--- app/tickets/layout.tsx
+++ app/tickets/layout.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import {IoMailOutline} from 'react-icons/io5';
 import {IoNotificationsOutline} from 'react-icons/io5';
+import BirdLogoMini from '@/public/auth/angkasa-logo-mini.png';
 
 export default function TicketLayout({children}: {
     children: React.ReactNode
@@ -10,15 +11,20 @@ export default function TicketLayout({children}: {
     return (
         <html>
             <body>
-                <nav className='flex flex-row container lg:gap-1'>
-                    <h1 className="lg:ml-5">Angkasa</h1>
-                    <input type="search" className="lg:ml-96" placeholder="Where you want to go?"/>
-                    <Link href="/tickets/findticket">
-                        Find Tickets
+                <nav className='flex flex-row lg:p-10'>
+                    <Link href='/'>
+                        <Image src={BirdLogoMini} alt="angkasa logo" width="120" height="88" />
                     </Link>
-                    <Link href="#">
-                        My Bookings
-                    </Link>
+
+                    <div className='lg:gap-1'>
+                        <input type="search" className="lg:ml-60 lg:mr-5 bg-gray-200 lg:w-80 lg:h-10 rounded-xl border-xl" placeholder="Where you want to go?"/>
+                        <Link href="/tickets/findticket" className="lg:hover:border-b-2 lg:hover:border-b-blue-800">
+                            Find Tickets
+                        </Link>
+                        <Link href="#" className="lg:ml-10 lg:hover:border-b-2 lg:hover:border-b-blue-800">
+                            My Bookings
+                        </Link>
+                    </div>
 
                     <div className="flex flex-row lg:ml-44 lg:gap-5">
                         <Link href="#">
@@ -28,13 +34,16 @@ export default function TicketLayout({children}: {
                             <IoNotificationsOutline size={30}/>
                         </Link>
                         <Link href="#">
-                            <Image className='rounded-xl' src="https://avatars.githubusercontent.com/u/70041921?v=4" alt="your profile" width="60" height="60" />
+                            <Image className='rounded-full' src="https://avatars.githubusercontent.com/u/70041921?v=4" alt="your profile" width="40" height="40" />
                         </Link>
                     </div>
                 </nav>
                 <div>
                     {children}
                 </div>
+                <footer>
+                    <h1>Angkasa Pura</h1>
+                </footer>
             </body>
         </html>
     )
